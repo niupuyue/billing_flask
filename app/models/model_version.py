d5f0abfb2fe974a2dcc1e66156b1544d5d0c376a
niupuyue <niupuyue@aliyun.com>
2023-05-14 20:42:25 +0800

    添加更新接口

--- app/models/model_version.py
+++ app/models/model_version.py
@@ -10,4 +10,5 @@ class VersionModel(db.Model):
     update_at = db.Column(db.DateTime, default=datetime.datetime.now, onupdate=datetime.datetime.now, comment='创建时间')
     platform = db.Column(db.Integer, default=0, comment='版本的平台 0:web,1:weiChat,2:Android,3:IOS')
     version_name = db.Column(db.String(20), default='000.000.000', comment='对应的版本号')
+    version_code = db.Column(db.Integer, default=0, comment='版本号的数字')
     enable = db.Column(db.Integer, default=0, comment='启用')
--- app/views/util/base.py
+++ app/views/util/base.py
@@ -1,15 +1,26 @@
 # 系统配置相关操作
 
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from sqlalchemy import func
 
-from app.models import BillType
+from app.extensions.init_sqlalchemy import db
+from app.models import BillType, VersionModel
 
 util_blueprint = Blueprint('util', __name__, url_prefix='/utils')
 
 
 @util_blueprint.get('/version_check/')
 def version_vercheck():
-    return "hello"
+    # 获取传递过来的版本号
+    version_code = int(request.args.get("version_code"))
+    # 找到最新的版本号
+    version_max_bean = VersionModel.query.order_by(VersionModel.version_code.desc()).first()
+    print("version_max_bean={}".format(version_max_bean))
+    result = {
+        "version_max": version_max_bean.version_code,
+        "need_update": version_code < version_max_bean.version_code
+    }
+    return jsonify(success=True, msg="获取成功~", data=result)
 
 
 @util_blueprint.get('/bill_type/test/')
